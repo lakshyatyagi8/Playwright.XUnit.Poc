c0959adc6cfe7f92f0660172f2d4713cc3873e3d
LAKSHWORKPC\laksh <lakshyatyagi8@gmail.com>
2026-07-27 17:33:48 +0100

    BasePage class refactoring

---
+++ PlaywrightAutomationPoc/GoogleMaps/Pages/BasePage.cs
@@ -0,0 +1,52 @@
+using Microsoft.Playwright;
+
+namespace PlaywrightAutomationPoc.GoogleMaps.Pages
+{
+    public class BasePage
+    {
+        /// <summary>
+        /// The Playwright page instance used for interacting with the Google Maps web page.
+        /// </summary>
+        protected readonly IPage _page;
+
+        // Locators are defined as properties (Encapsulation)
+        protected ILocator SearchBox => _page.Locator("input[name='q']");
+        
+        protected ILocator SearchButton => _page.GetByRole(AriaRole.Button, new() { Name = "Search" });
+        
+        protected ILocator CookieButton => _page.GetByRole(AriaRole.Button, new() { Name = "Accept all" });
+
+        protected BasePage(IPage page)
+        {
+            _page = page;
+        }
+
+        /// <summary>
+        /// Navigates to the specified base URL.
+        /// </summary>
+        /// <param name="baseUrl"></param>
+        /// <returns></returns>
+        public async Task NavigateAsync(string baseUrl)
+        {
+            await _page.GotoAsync(baseUrl);
+        }
+
+        /// <summary>
+        /// Handles cookie consent by clicking the accept button if it appears.
+        /// </summary>
+        /// <returns></returns>
+        public async Task HandleCookiesAsync()
+        {
+            // Fail-safe: Only click if it appears (timeout reduced to avoid long waits)
+            try 
+            {
+                if (await CookieButton.IsVisibleAsync())
+                {
+                    await CookieButton.ClickAsync();
+                }
+            } 
+            catch { /* Ignore if cookie banner doesn't appear */ }
+        }
+
+    }
+}
--- PlaywrightAutomationPoc/GoogleMaps/Pages/MapsPage.cs
+++ PlaywrightAutomationPoc/GoogleMaps/Pages/MapsPage.cs
@@ -3,59 +3,21 @@ using PlaywrightAutomationPoc.AutoFramework.Extensions;
 
 namespace PlaywrightAutomationPoc.GoogleMaps.Pages
 {
-    public class MapsPage : IMapsPage
+    public class MapsPage : BasePage
     {
-        /// <summary>
-        /// The Playwright page instance used for interacting with the Google Maps web page.
-        /// </summary>
-        private readonly IPage _page;
-
-        // Locators are defined as properties (Encapsulation)
-        private ILocator SearchBox => _page.Locator("input[name='q']");
-        
         private ILocator Directions => _page.GetByRole(AriaRole.Button, new() { Name = "Directions" });
 
         private ILocator StartingPoint => _page.GetByPlaceholder("Choose starting point, or click on the map...");
-        private ILocator AddDestination => _page.Locator("div[class='d2cEI'] span[class='ExQYxb google-symbols']");
-
-        private ILocator SearchButton => _page.GetByRole(AriaRole.Button, new() { Name = "Search" });
         // Selects the first result that looks like a location link
         private ILocator FirstResult => _page.Locator("a[href*='/place/']").First; 
-        private ILocator CookieButton => _page.GetByRole(AriaRole.Button, new() { Name = "Accept all" });
         private ILocator Headline(string sDestination) => _page.Locator($"div[aria-label*='{sDestination}']").First;
-        private ILocator SearchStopOption => _page.Locator("//div[@class='KG8wXc fontBodyMedium']");
         private ILocator RouteOption1Time => _page.Locator("//div[@id='section-directions-trip-0']//div[contains(@class,'Fk3sm fontHeadlineSmall')]");
-
-        public MapsPage(IPage page)
+        
+        // 2. Constructor
+        // Passes the IPage instance up to the BasePage
+        public MapsPage(IPage page) : base(page)
         {
-            _page = page;
-        }
-
-        /// <summary>
-        /// Navigates to the specified base URL.
-        /// </summary>
-        /// <param name="baseUrl"></param>
-        /// <returns></returns>
-        public async Task NavigateAsync(string baseUrl)
-        {
-            await _page.GotoAsync(baseUrl);
-        }
-
-        /// <summary>
-        /// Handles cookie consent by clicking the accept button if it appears.
-        /// </summary>
-        /// <returns></returns>
-        public async Task HandleCookiesAsync()
-        {
-            // Fail-safe: Only click if it appears (timeout reduced to avoid long waits)
-            try 
-            {
-                if (await CookieButton.IsVisibleAsync())
-                {
-                    await CookieButton.ClickAsync();
-                }
-            } 
-            catch { /* Ignore if cookie banner doesn't appear */ }
+            
         }
 
         /// <summary>
@@ -65,7 +27,6 @@ namespace PlaywrightAutomationPoc.GoogleMaps.Pages
         /// <returns></returns>
         public async Task SearchLocationAsync(string locationName)
         {
-            await SearchBox.WaitForAsync(); // Explicit wait for stability
             await SearchBox.FillAsync(locationName);
             await SearchButton.ClickAsync();
         }
